8563517796934d09de854b85f0a1e2e182a33bd7
AleksandrTheGreat <267179@bk.ru>
2023-12-01 18:14:40 +0500

    first form (on main page)

--- slib/views.py
+++ slib/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .forms import *
 
 def main(request):
-    data = {}
+    poisk = search()
+    data = {'poisk':poisk}
     return render(request,'main.html', context=data)
